32f5e39a25a18a5b8e6bba1f7ac8c8f6fbbec0f4
lotusmeanseight <g.goering@ostfalia.de>
2019-04-04 19:54:54 +0200

    just added implements, otherwise reddit still stub

--- src/main/java/com/github/lotusmeanseight/sources/impl/reddit/Reddit.java
+++ src/main/java/com/github/lotusmeanseight/sources/impl/reddit/Reddit.java
@@ -1,4 +1,42 @@
 package com.github.lotusmeanseight.sources.impl.reddit;
 
-public class Reddit {
+import com.github.lotusmeanseight.entry.NewsEntry;
+import com.github.lotusmeanseight.sources.RedditSource;
+
+import java.util.List;
+
+public class Reddit implements RedditSource {
+
+    public List<NewsEntry> getControversialEntries(int numberOfEntries) {
+        return null;
+    }
+
+    public List<NewsEntry> getControversialEntriesByCategory(int numberOfEntries, String category) {
+        return null;
+    }
+
+    public List<NewsEntry> getRisingEntries(int numberOfEntries) {
+        return null;
+    }
+
+    public List<NewsEntry> getRisingEntriesByCategory(int numberOfEntries, String category) {
+        return null;
+    }
+
+    public List<NewsEntry> getTopEntries(int numberOfEntries) {
+        return null;
+    }
+
+    public List<NewsEntry> getTopEntriesByCategory(int numberOfEntries, String category) {
+        return null;
+    }
+
+    public List<NewsEntry> getNewEntries(int numberOfEntries) {
+        return null;
+    }
+
+    public List<NewsEntry> getNewEntriesByCategory(int numberOfEntries, String category) {
+        return null;
+    }
+
 }
