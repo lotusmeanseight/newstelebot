07f28fea3b6efa893b78fe47b7c830f4138048e8
lotusmeanseight <g.goering@ostfalia.de>
2019-04-11 13:00:02 +0200

    config change test addition (sort of) factory pattern needs to be changed into abstract factory pattern

--- src/test/java/com/github/lotusmeanseight/service/RedditSimpleServiceIT.java
+++ src/test/java/com/github/lotusmeanseight/service/RedditSimpleServiceIT.java
@@ -1,8 +1,33 @@
 package com.github.lotusmeanseight.service;
 
+import com.github.lotusmeanseight.entry.NewsEntry;
+import com.github.lotusmeanseight.source.entrycollection.EntryCollectionFactory;
+import com.github.lotusmeanseight.source.entrycollection.reddit.RedditEntryCollection;
+import com.github.lotusmeanseight.source.entrycollection.reddit.RedditSimpleEntryCollection;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RedditSimpleServiceIT {
+
+    private int numberOfEntries;
+    private List<NewsEntry> entryList;
+    private EntryCollectionFactory factory;
+    private RedditEntryCollection simpleEntryCollection;
+
+    @BeforeAll
+    public void setup(){
+        this.numberOfEntries = 5;
+        entryList = new ArrayList<>();
+    }
+
+    @AfterEach
+    public void resetList(){
+        entryList = new ArrayList<>();
+    }
 
     @Test
     public void testControversialEntries(){
